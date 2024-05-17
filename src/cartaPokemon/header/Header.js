889eb00f7c52edc069bbeef38d5644d9b1da450d
itallo ferreira <italloferreira2208@gmail.com>
2024-05-17 18:21:28 -0300

    Pokemon Card

---
+++ src/cartaPokemon/header/Header.js
@@ -0,0 +1,17 @@
+import './Header.css';
+
+function Header() {
+    return (
+    <>
+    <div>
+
+      <h1>QUEM É ESSE <p>POKEMON</p> ? </h1>
+
+    </div>
+    
+    </>
+    );
+  }
+  
+  export default Header;
+  
